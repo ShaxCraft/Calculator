c60d2f4544282d18285e8d837c273a85b2bb4ce6
Shax <ShaxCraft@gmail.com>
2019-03-17 16:35:52 +0200

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         Parser parser = new Parser();
+
         Data data = parser.parse(args);
         int result = calculator.calc(data);
         System.out.println(result);
